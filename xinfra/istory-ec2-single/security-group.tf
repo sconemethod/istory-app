440d02fdfa1f38455d66c512f68874dd574c456c
scone <sconemethod@gmail.com>
2025-03-22 17:05:36 +0900

    scaling workflow

--- xinfra/istory-ec2-single/security-group.tf
+++ xinfra/istory-ec2-single/security-group.tf
@@ -1,31 +1,3 @@
-resource "aws_security_group" "istory_nginx_sg" {
-  name_prefix = "istory nginx sg"
-  vpc_id      = aws_vpc.dangtong-vpc.id 
-
-  ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow HTTP"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 # RDS 보안 그룹 추가
 resource "aws_security_group" "istory_rds_sg" {
   name_prefix = "istory rds sg"
@@ -36,7 +8,10 @@ resource "aws_security_group" "istory_rds_sg" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [aws_security_group.istory_nginx_sg.id]
+    security_groups = [
+      aws_security_group.istory_nginx_sg.id,
+      aws_security_group.istory_prod_ec2_sg.id 
+      ]
   }
 
   egress {
